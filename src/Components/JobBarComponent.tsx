855fbd26c8c079db6390592ca16afa8f87c35557
ChristmasLover12345 <madnes0205@gmail.com>
2025-04-08 15:44:16 -0700

    trying to make a section

--- src/Components/JobBarComponent.tsx
+++ src/Components/JobBarComponent.tsx
@@ -1,12 +1,16 @@
 'use client'
 
-import { JobBar } from '@/utils/interface'
+
 import React from 'react'
 
-const JobBarComponent = (props: {background: string, imageOnRight: boolean,}) => {
+const JobBarComponent = (props: {background: string, imageOnRight: boolean, LogoUrl: string,}) => {
   return (
     
-   <div className={`flex flex-row ${"bg-[#" + props.background + "]" } ${props.imageOnRight ? 'xl:flex-row-reverse' : 'xl:flex-row'}`}>
+   <div className={`relative flex w-full flex-row ${"bg-[#" + props.background + "]" } ${props.imageOnRight ? 'xl:flex-row-reverse' : 'xl:flex-row'}`}>
+
+    <img src={props.LogoUrl} alt="logo" className={`absolute h-[48px] w-[48px] ${props.imageOnRight ? 'end-1 top-1' : 'start-1 top-1'}`}/>
+
+    
 
    </div>
 
